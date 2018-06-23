b1ad26c943c33d2185b0cdf12bb177128317a775
Nate <nathanperfetti@gmail.com>
2018-06-23 11:59:37 -0500

    Initial commit with file structure and some boilerplate code requiring relevant pkgs

---
+++ controllers/poems_controllers.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var router = express.Router();
+
+
+
+
+
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+const routes = require("./controllers/goals_controllers");
+app.use(routes)
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
