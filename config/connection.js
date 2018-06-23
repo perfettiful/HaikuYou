3b1d05cedb7ccd2f66e8d41e547e9e8e19301f82
Nate <nathanperfetti@gmail.com>
2018-06-23 13:53:36 -0500

    Added html template for submition form

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8889,
-  user: "root",
-  password: "root",
-  database: "poemss_db"
+  port: "",
+  user: "",
+  password: "",
+  database: ""
 });
 
 connection.connect(function(err) {
