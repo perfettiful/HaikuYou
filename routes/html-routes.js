0178e78a86293360cfa99ffdc031a8b2d46cd811
Robyn Craig <robynocraig@gmail.com>
2018-06-27 21:05:18 -0500

    Set up api & html routes, along with poem model

---
+++ routes/html-routes.js
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/home.html"));
+  });
+
+  app.get("/compose", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/freeform.html"));
+  });
+
+};
