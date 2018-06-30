969b970edcb7a957fc21b01f90cd93ece7892e12
Robyn Craig <robynocraig@gmail.com>
2018-06-30 12:14:18 -0500

    Added validation for syllables on form

--- models/haiku.js
+++ models/haiku.js
@@ -1,9 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
   var Haiku = sequelize.define("Haiku", {
     haiku_title: DataTypes.STRING,
-    haiku_lineA: DataTypes.STRING,
-    haiku_lineB: DataTypes.STRING,
-    haiku_lineC: DataTypes.STRING,
+    haiku_lineA: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    haiku_lineB: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    haiku_lineC: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     haiku_author: DataTypes.STRING,
     haiku_category: DataTypes.STRING,
   });
